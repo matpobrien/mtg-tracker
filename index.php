b31c5f6845117dfd6710216f3c82884ab4ed6e03
Mat <matpobrien@gmail.com>
2024-08-02 16:43:36 +0200

    trying stuff

--- index.php
+++ index.php
@@ -4,7 +4,10 @@ include_once __DIR__ . '/Templates/MainTemplate.php';
 include_once __DIR__ . '/GameController.php';
 include_once __DIR__ . '/GameRepository.php';
 
-$gameRepository = new GameRepository();
+$config = [
+    'fileName' => __DIR__ . '/games.json',
+];
+$gameRepository = new GameRepository($config['fileName']);
 $gameController = new GameController($gameRepository);
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
