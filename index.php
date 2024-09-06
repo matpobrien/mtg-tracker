56cd8dcde8d12c3a9c72582bedd329412c930ddb
Mat <matthew.obrien@lipsia.digital>
2024-09-06 10:35:27 +0200

    trying stuff

--- index.php
+++ index.php
@@ -28,6 +28,7 @@ if (isset($_POST['login']) || isset($_POST['signup'])) {
 }
 echo '<p>'. $authController->getCurrentUser()->getUsername() . '</p>';
 $authenticated = $authController->isAuthenticated($config['loggedIn']);
+echo '<p>' . $authenticated . '</p>';
 if (!$authenticated) {
     $authController->renderLogin();
 }
