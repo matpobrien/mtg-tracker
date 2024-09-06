b387cc1ee864ca9c2042ff21bbe25c6e63d6a29c
Mat <matthew.obrien@lipsia.digital>
2024-09-06 15:05:53 +0200

    signout button

--- index.php
+++ index.php
@@ -24,7 +24,6 @@ $authenticated = $authController->isAuthenticated($config['loggedIn']);
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (isset($_POST['signup'])) {
         $config['newUser'] = !$authController->signup();
-        $authenticated = $authController->isAuthenticated($config['loggedIn']);
         if ($authenticated) {
             echo $gameController->getGames();
         }
@@ -32,12 +31,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (isset($_POST['login'])) {
         $loggedIn = $authController->login();
         $config['newUser'] = !$loggedIn;
-        $authenticated = $authController->isAuthenticated($config['loggedIn']);
     }
     if ($authenticated) {
         if (isset($_POST['signout'])) {
             $authController->signout();
-            $authenticated = $authController->isAuthenticated($config['loggedIn']);
         }
         if (isset($_POST['addGame'])) {
             echo $gameController->addGame();
