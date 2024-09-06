eeb54b7141e63035b13fa2174a5e0d07dddc7650
Mat <matthew.obrien@lipsia.digital>
2024-09-06 12:58:29 +0200

    trying stuff

--- Service/AuthenticationService.php
+++ Service/AuthenticationService.php
@@ -12,6 +12,7 @@ class AuthenticationService
     public function authenticate(string $username, string $password): bool
     {
         $user = $this->userRepository->findUserByUsername($username);
+        echo '<p>' . json_encode(['user' => $user]) . '</p>';
         
         if (null === $user) {
             return false;
