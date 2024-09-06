4f5d58bd62925d5fd9acf256435a7fb05b2ecca7
Mat <matthew.obrien@lipsia.digital>
2024-09-06 13:30:13 +0200

    trying stuff

--- Service/AuthenticationService.php
+++ Service/AuthenticationService.php
@@ -17,9 +17,7 @@ class AuthenticationService
             return false;
         }
         
-        echo '<p>' . json_encode(['passwordVerify' => strcmp($password, $user->getPassword()) === 0]) . '</p>';
-        
-        if (password_verify($password, $user->getPassword())) {
+        if (0 === strcmp($password, $user->getPassword())) {
             return setcookie('jwt', $this->generateJwt($username));
         }
         return false;
