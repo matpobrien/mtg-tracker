f18f87ad5555ab357cb500ce360761c56062eea0
Mat <matthew.obrien@lipsia.digital>
2024-09-06 10:42:33 +0200

    trying stuff

--- index.php
+++ index.php
@@ -29,7 +29,8 @@ if (isset($_POST['login']) || isset($_POST['signup'])) {
 echo '<p> User:'. $authController->getCurrentUser()->getUsername() . '</p>';
 $authenticated = $authController->isAuthenticated($config['loggedIn']);
 echo '<p> Authenticated:' . $authenticated . '</p>';
-echo '<p> Config logged in' . $config['loggedIn'] . '</p>';
+echo '<p> Config logged in:' . $config['loggedIn'] . '</p>';
+echo '<p> Config:' . json_encode($config) . '</p>';
 echo '<p> Cookie isset:' . isset($_COOKIE['jwt']) . '</p>';
 if (!$authenticated) {
     $authController->renderLogin();
